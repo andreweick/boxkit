fad68a4de580862af0e78468a7f3f49a35a4da35
Andrew Eick <andy@eick.com>
2023-12-28 13:06:26 -0500

    Create add-1p-repo.sh

---
+++ add-1p-repo.sh
@@ -0,0 +1,2 @@
+echo https://downloads.1password.com/linux/alpinelinux/stable/ >> /etc/apk/repositories
+wget https://downloads.1password.com/linux/keys/alpinelinux/support@1password.com-61ddfc31.rsa.pub -P /etc/apk/keys
